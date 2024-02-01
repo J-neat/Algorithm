e793d8273060bb76af2912b6baf95dc95530b16c
J-neat <115057370+J-neat@users.noreply.github.com>
2024-02-01 22:38:46 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,12 @@
+def solution(hp):
+    answer = 0
+    if hp % 5 >= 0:
+        answer += (hp//5)
+        hp = hp%5
+        if hp %3 >= 0:
+            answer += (hp//3)
+            hp = hp%3
+            if hp >= 0:
+                answer += hp
+            
+    return answer
