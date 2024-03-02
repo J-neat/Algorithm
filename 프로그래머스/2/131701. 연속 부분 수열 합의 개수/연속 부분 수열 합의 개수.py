900fe914f4fe0f2d7784d16701122a80c21f2e5c
J-neat <115057370+J-neat@users.noreply.github.com>
2024-03-02 16:13:53 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4847.47 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,12 @@
+def solution(elements):
+    result = set()
+    n = 0
+    while n != len(elements):
+        n += 1
+        for i in range(len(elements)):
+            if i+n > len(elements):
+                result.add(sum(elements[i:]+elements[:i+n-len(elements)]))
+            else:
+                result.add(sum(elements[i:i+n]))
+    
+    return len(result)
