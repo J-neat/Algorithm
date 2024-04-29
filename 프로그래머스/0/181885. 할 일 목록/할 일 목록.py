a564fc1ccc3546fd6ba8f684cf801680c51dfecd
J-neat <115057370+J-neat@users.noreply.github.com>
2024-04-29 15:23:17 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,8 @@
+def solution(todo_list, finished):
+    answer = []
+    for i in range(len(todo_list)):
+        if finished[i] == 0:
+            answer.append(todo_list[i])
+                
+                
+    return answer
