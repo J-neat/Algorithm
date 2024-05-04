054f8cbbfe16b45090c4d283f66b88281abe8242
J-neat <115057370+J-neat@users.noreply.github.com>
2024-05-05 01:59:28 +0900

    [level 2] Title: 타겟 넘버, Time: 3.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -5,10 +5,8 @@ def solution(numbers, target):
 
 def dfs(numbers, target, current, idx):
     global cnt
-    if idx == len(numbers):
-        if current == target:
-            cnt += 1
-        return
-    else:
+    if target == current and idx == len(numbers):
+        cnt += 1
+    elif idx != len(numbers):
         dfs(numbers, target, current + numbers[idx], idx+1)
         dfs(numbers, target, current - numbers[idx], idx+1)
