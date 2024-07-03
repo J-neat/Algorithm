857c2bddcd8f571f2d28308999598513d5109dc9
J-neat <115057370+J-neat@users.noreply.github.com>
2024-07-03 16:00:42 +0900

    [level 0] Title: 배열 만들기 6, Time: 12.41 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    stk = []
+    for i in range(len(arr)):
+        if stk and stk[-1] == arr[i]:
+            stk.pop()
+        else:
+            stk.append(arr[i])
+    return stk or [-1]
