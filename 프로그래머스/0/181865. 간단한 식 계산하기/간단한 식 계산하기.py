458cfa6d5896016cfdb1b9fd8cf910bb5dd72a54
J-neat <115057370+J-neat@users.noreply.github.com>
2024-07-31 20:24:34 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,6 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+    if op == '+': return int(a) + int(b)    
+    if op == '-': return int(a) - int(b)
+    if op == '*': return int(a) * int(b)    
+    if op == '/': return int(a) / int(b)
